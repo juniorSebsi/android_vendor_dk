1fb06c5cdb1f36276929c18476d2e04211255b15
XXMrHyde <xxmrhyde@gmx.co.uk>
2013-11-12 00:04:34 +0100

    Add default sounds overrides

--- config/common.mk
+++ config/common.mk
@@ -1,7 +1,12 @@
-# bootanimation
-
+# Bootanimation
 PRODUCT_COPY_FILES += \
   vendor/dk/bootanimation.zip:system/media/bootanimation.zip
+
+# Sounds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=Playa.ogg \
+    ro.config.notification_sound=regulus.ogg \
+    ro.config.alarm_alert=Alarm_Beep_03.ogg
 
 # Additional packages
 -include vendor/dk/config/packages.mk
