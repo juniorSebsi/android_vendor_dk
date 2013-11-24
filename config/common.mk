5348070e46b1736ed786b6486f0e899fae7a51ae
XXMrHyde <xxmrhyde@gmx.co.uk>
2013-11-24 01:35:47 +0100

    Add Backup function and init.d support:
    Based on CyanogenMod

--- config/common.mk
+++ config/common.mk
@@ -8,6 +8,21 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.notification_sound=regulus.ogg \
     ro.config.alarm_alert=Alarm_Beep_03.ogg
 
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/dk/prebuilt/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/dk/prebuilt/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/dk/prebuilt/bin/50-hosts.sh:system/addon.d/50-hosts.sh \
+    vendor/dk/prebuilt/bin/blacklist:system/addon.d/blacklist
+
+# init.d support
+PRODUCT_COPY_FILES += \
+        vendor/dk/prebuilt/bin/sysinit:system/bin/sysinit
+
+# Init script file with DarkKat extras
+PRODUCT_COPY_FILES += \
+    vendor/dk/prebuilt/etc/init.local.rc:root/init.dk.rc
+
 # Additional packages
 -include vendor/dk/config/packages.mk
 
