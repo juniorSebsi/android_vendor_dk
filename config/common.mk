55ea7bea4d1cf42f880102f7be5dbce121f7be3e
XXMrHyde <xxmrhyde@gmx.co.uk>
2014-03-25 15:49:27 +0100

    Use CyanogenMod cLock as default lock screen widget:
    Lock screen widgets has to be enabled at:
    System settings -> Security -> (Enable widgets)

--- config/common.mk
+++ config/common.mk
@@ -26,5 +26,7 @@ PRODUCT_COPY_FILES += \
 # Additional packages
 -include vendor/dk/config/packages.mk
 
+PRODUCT_PACKAGE_OVERLAYS += vendor/dk/overlay
+
 # Versionning
 -include vendor/dk/config/version.mk
