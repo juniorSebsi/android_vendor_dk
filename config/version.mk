9e0f680e1cfc8c1f7e817004d35d6b41ea583565
XXMrHyde <xxmrhyde@gmx.co.uk>
2014-09-02 22:50:55 +0200

    Change OTA url

--- config/version.mk
+++ config/version.mk
@@ -2,9 +2,9 @@
  ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(subst dk_,,$(TARGET_PRODUCT))
 
 # General update server uri
- GENERAL_SERVER_URI := http://roms.test84425.test-account.com/
+ GENERAL_SERVER_URI := https://dl.dropboxusercontent.com/s/95n6sp8fk785746/
 
  # Apply it to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=DarkKat-$(ROM_VERSION)\
-    dk.updater.uri=$(GENERAL_SERVER_URI)$(TARGET_PRODUCT).json
+    dk.updater.uri=$(GENERAL_SERVER_URI)$(TARGET_PRODUCT)_new.json
