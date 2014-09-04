23a9c52897e1821e8660e8629f50fddb3ec3c275
XXMrHyde <xxmrhyde@gmx.co.uk>
2014-09-05 01:17:43 +0200

    Update OTA updater uri for all official supported devices, (4/4)

--- config/version.mk
+++ config/version.mk
@@ -1,10 +1,10 @@
 # Versioning of the ROM
  ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(subst dk_,,$(TARGET_PRODUCT))
 
-# General update server uri
- GENERAL_SERVER_URI := https://dl.dropboxusercontent.com/s/95n6sp8fk785746/
+# General part of the OTA update uri
+ GENERAL_UPDATE_URI_PART := https://dl.dropboxusercontent.com/s/
 
  # Apply it to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=DarkKat-$(ROM_VERSION)\
-    dk.updater.uri=$(GENERAL_SERVER_URI)$(TARGET_PRODUCT)_new.json
+    dk.updater.uri=$(GENERAL_UPDATE_URI_PART)$(PRODUCT_UPDATE_URI_PART)
