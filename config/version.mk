b1d186d64fc3bbfcf02e315952131e32a55987e6
XXMrHyde <xxmrhyde@gmx.co.uk>
2014-11-09 15:37:19 +0100

    Print DARKKAT_VERSION in build config, (2/2)

--- config/version.mk
+++ config/version.mk
@@ -1,5 +1,6 @@
 # Versioning of the ROM
  ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d)-$(DK_RELEASE_TYPE)-$(subst dk_,,$(TARGET_PRODUCT))
+ DK_VERSION := $(PLATFORM_VERSION)-$(DK_RELEASE_TYPE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.dk.version=$(ROM_VERSION)
